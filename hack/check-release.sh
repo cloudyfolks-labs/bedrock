b1ab6c12a5e12ba2047f3d54f1d2ca4e4228aed7
Taha Farahani <tahaisworking@gmail.com>
2026-09-19 15:07:31 +0330

    feat(release): add kured for managed hosts
    Signed-off-by: Taha Farahani <tahaisworking@gmail.com>

--- hack/check-release.sh
+++ hack/check-release.sh
@@ -5,7 +5,7 @@ dir=${1:-dist/release}
 
 test -f "$dir/release.yaml"
 test -f "$dir/images.txt"
-for group in 00-crds 10-kube-vip 20-fabric 60-cert-manager 70-monitoring 90-bedrock; do
+for group in 00-crds 10-kube-vip 20-fabric 60-cert-manager 70-monitoring 80-kured 90-bedrock; do
   test -d "$dir/manifests/$group"
 done
 grep -q 'kind: CustomResourceDefinition' "$dir/manifests/00-crds/cert-manager-crds.yaml"
@@ -15,6 +15,7 @@ grep -q 'BEDROCK_MASTER_IPS' "$dir/manifests/20-fabric/fabric.yaml"
 grep -q 'kind: CustomResourceDefinition' "$dir/manifests/00-crds/fabric-crds.yaml"
 grep -q 'name: cert-manager' "$dir/manifests/60-cert-manager/cert-manager.yaml"
 grep -q 'metrics-server' "$dir/manifests/70-monitoring/monitoring.yaml"
+grep -q 'kind: DaemonSet' "$dir/manifests/80-kured/kured.yaml"
 grep -q 'name: bedrock-operator' "$dir/manifests/90-bedrock/bedrock.yaml"
 grep -q 'kind: ValidatingAdmissionPolicy' "$dir/manifests/90-bedrock/bedrock.yaml"
 ! grep -rq 'ghcr.io/cloudyfolks-labs/bedrock:dev' "$dir/manifests" || test "$(grep '^version:' "$dir/release.yaml")" = "version: dev"
