06119bab6b4d584dafb0ab207549973c2451bc11
Taha Farahani <tahaisworking@gmail.com>
2026-09-21 03:50:58 +0330

    feat(release): add rook ceph and csi drivers
    Signed-off-by: Taha Farahani <tahaisworking@gmail.com>

--- hack/check-release.sh
+++ hack/check-release.sh
@@ -5,7 +5,7 @@ dir=${1:-dist/release}
 
 test -f "$dir/release.yaml"
 test -f "$dir/images.txt"
-for group in 00-crds 10-kube-vip 20-fabric 25-multus 30-nmstate 60-cert-manager 70-monitoring 80-kured 90-bedrock; do
+for group in 00-crds 10-kube-vip 20-fabric 25-multus 30-nmstate 40-rook 41-rook-csi 60-cert-manager 70-monitoring 80-kured 90-bedrock; do
   test -d "$dir/manifests/$group"
 done
 grep -q 'kind: CustomResourceDefinition' "$dir/manifests/00-crds/cert-manager-crds.yaml"
@@ -21,6 +21,16 @@ grep -q 'name: nmstate-operator' "$dir/manifests/30-nmstate/nmstate.yaml"
 grep -q 'kind: NMState' "$dir/manifests/30-nmstate/nmstate.yaml"
 grep -q 'name: nmstates.nmstate.io' "$dir/manifests/00-crds/nmstate-crds.yaml"
 grep -q 'kubernetes-nmstate-handler:v0.87.0' "$dir/images.txt"
+grep -q 'name: rook-ceph-operator' "$dir/manifests/40-rook/rook.yaml"
+grep -q 'name: snapshot-controller' "$dir/manifests/40-rook/rook.yaml"
+grep -q 'snapshot-controller:v8.5.0' "$dir/manifests/40-rook/rook.yaml"
+grep -q 'name: cephclusters.ceph.rook.io' "$dir/manifests/00-crds/rook-crds.yaml"
+grep -q 'name: volumesnapshotclasses.snapshot.storage.k8s.io' "$dir/manifests/00-crds/rook-crds.yaml"
+grep -q 'name: drivers.csi.ceph.io' "$dir/manifests/00-crds/rook-crds.yaml"
+grep -q 'kind: Driver' "$dir/manifests/41-rook-csi/rook-csi.yaml"
+grep -q 'quay.io/ceph/ceph:v20.2.4' "$dir/images.txt"
+grep -q 'quay.io/cephcsi/cephcsi:v3.17.1' "$dir/images.txt"
+grep -q 'image: quay.io/ceph/ceph:v20.2.4' "$dir/release.yaml"
 grep -q 'name: cert-manager' "$dir/manifests/60-cert-manager/cert-manager.yaml"
 grep -q 'metrics-server' "$dir/manifests/70-monitoring/monitoring.yaml"
 grep -q 'kind: DaemonSet' "$dir/manifests/80-kured/kured.yaml"
